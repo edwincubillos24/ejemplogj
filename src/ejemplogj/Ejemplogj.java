e05ddd9592386ad1a51481a67e00d954869c1844
Edwin <edwinacubillos@gmail.com>
2016-02-22 17:00:44 -0500

    version 2.0 Segunda conexion

--- src/ejemplogj/Ejemplogj.java
+++ src/ejemplogj/Ejemplogj.java
@@ -15,7 +15,7 @@ public class Ejemplogj {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int numero;// TODO code application logic here
     }
     
 }
